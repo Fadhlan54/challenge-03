5976d6cb3843d8ccab145fa7734a63f0c6bd7c39
Fadhlan54 <muhammadfadhlan0011@gmail.com>
2023-09-21 04:35:42 +0700

    me-refactor dan memindahkan function ke folder utils

--- index.js
+++ index.js
@@ -1,128 +1,35 @@
-const express = require('express');
-const fs = require('fs');
+// internal module
+const {
+  pingServer,
+  getAllCarsData,
+  getCarDataByID,
+  createCarData,
+  editCarData,
+  deleteCarData,
+} = require(`${__dirname}/utils/carUtils`);
+
+// third-party module
+const express = require("express");
+const app = express();
+
 const port = 8000;
 
-const app = express();
 app.use(express.json());
 
-const dataPath = `${__dirname}/data/cars.json`;
-const carList = JSON.parse(fs.readFileSync(dataPath));
+const carRoute = express.Router();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'ping succesfully'
-    })
-});
+app.get("/", pingServer);
 
-app.get('/cars', (req, res) => {
-    if (!carList) {
-        return res.json({
-            status: 'failed',
-            message: 'data not found'
-        })
-    }
-    res.json({
-        status: 'success',
-        data: carList
-    });
-});
+carRoute.route("/").get(getAllCarsData).post(createCarData);
 
-app.get('/cars/:id', (req, res) => {
-    const findCarByID = carList.find(car => car.id == req.params.id);
-    if (findCarByID) {
-        res.status(200).json({
-            status: 'success',
-            data: findCarByID
-        });
-    } else {
-        res.status(404).json({
-            status: 'failed',
-            message: "Tidak ada mobil yang sesuai dengan id diberikan"
-        });
-    }
-});
+carRoute
+  .route("/:id")
+  .get(getCarDataByID)
+  .put(editCarData)
+  .delete(deleteCarData);
 
-app.post('/cars', (req, res) => {
-    const newCar = req.body;
-
-    for (const car of carList) {
-        if (car.plate === newCar.plate) {
-            return res.status(409).json({
-                status: 'failed',
-                message: 'plat nomor yang diberikan sudah terdaftar'
-            });
-        }
-    }
-
-    carList.push(newCar);
-    fs.writeFile(dataPath, JSON.stringify(carList), err => {
-        if (err) {
-            return res.status(404).json({
-                status: 'failed',
-                message: 'Error ketika menulis data ke file json'
-            })
-        }
-        res.status(201).json({
-            status: 'success',
-            data: newCar
-        });
-    });
-});
-
-app.put('/cars/:id', (req, res) => {
-    const carId = req.params.id;
-    const updatedCar = req.body;
-    const index = carList.findIndex(car => car.id === carId);
-
-    if (index !== -1) {
-        carList[index] = updatedCar;
-        fs.writeFile(dataPath, JSON.stringify(carList), err => {
-            if (err) {
-                return res.status(404).json({
-                    status: 'failed',
-                    message: 'gagal menulis data ke dalam file json'
-                })
-            }
-            res.status(200).json({
-                status: 'success',
-                data: updatedCar
-            });
-        })
-
-    } else {
-        res.status(404).json({
-            status: 'failed',
-            message: "id mobil yang diberikan tidak ditemukan!"
-        });
-    }
-});
-
-app.delete('/cars/:id', (req, res) => {
-    const carId = req.params.id;
-    const index = carList.findIndex(car => car.id === carId);
-    if (index !== -1) {
-        const deletedCar = carList.splice(index, 1)[0];
-        fs.writeFile(dataPath, JSON.stringify(carList), err => {
-            if (err) {
-                return res.status(404).json({
-                    status: 'failed',
-                    message: 'gagal menulis data ke dalam file json'
-                });
-            }
-            res.status(200).json({
-                status: 'success',
-                data: deletedCar
-            });
-        })
-
-    } else {
-        res.status(404).json({
-            status: 'failed',
-            message: "id yang diberikan tidak ditemukan"
-        });
-    }
-});
+app.use("/cars", carRoute);
 
 app.listen(port, () => {
-    console.log(`server berjalan di port ${port}`);
-})
+  console.log(`server berjalan di port ${port}`);
+});
